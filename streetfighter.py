967ddaa5ff3aa274cfb8a40fed919949330a2226
Feather Feet <featherfeet5436@gmail.com>
2016-01-09 19:24:11 +0000

    Did some more moves

--- streetfighter.py
+++ streetfighter.py
@@ -10,9 +10,36 @@ img4=pygame.image.load('images/p4.png')
 
 l=[img0,img1,img2,img3,img4]'''
 l=[]
-for a in range(0,18,1):
+Jump=[]
+RunForward=[]
+Punch=[]
+Duck=[]
+Slide=[]
+SpecialMove=[]
+for a in range(0,19,1):
     l.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+    if a==7 or a==18:
+        Punch.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+    if a==6:
+        Jump.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+    if a==3 or a==4 or a==5:
+        RunForward.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
+    if a==9:
+        Duck.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
+    if a==8:
+        Slide.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+    if a==10:
+        SpecialMove.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+
 x=0
+r=0
+p=0
+s=0
+c=0
+d=0
+f=0
+q=0
+t=0
 clock=pygame.time.Clock()
 while True:
     clock.tick(5)
@@ -22,7 +49,84 @@ while True:
         if event.type==QUIT:
             pygame.quit()
             exit()
-    screen.blit(l[x],(540,320))
-    x=x+1
-    if x>=18:
-        x=0
+        if event.type==KEYDOWN:
+            if event.key==K_d:
+#                print('"Run or D" key pressed')
+                r=1
+            if event.key==K_e:
+#                print('"Punch or E" key pressed')
+                p=1
+            if event.key==K_SPACE:
+#                print('"Jump or SPACEBAR" key pressed')
+                s=1
+            if event.key==K_f:
+#                print('"Duck or F" key pressed')
+                f=1
+            if event.key==K_q:
+#                print('"Slide or q" is pressed')
+                q=1
+            if event.key==K_t:
+#                print('"SpecialMove or t" is pressed')
+                t=1
+##          if event.key==K_UP:
+##              print ('"Up" arrow" key pressed')
+        if event.type==KEYUP:
+            if event.key==K_d:
+#                print('"Run or D" key pressed')
+                r=0
+            if event.key==K_e:
+#                print('"Punch or E" key pressed')
+                p=0
+            if event.key==K_SPACE:
+#                print('"Jump or SPACEBAR" key pressed')
+                s=0
+            if event.key==K_f:
+#                print('"Duck or F" key pressed')
+                f=0
+            if event.key==K_q:
+#                print('"Slide or q" is pressed')
+                q=0
+            if event.key==K_t:
+#                print('"SpecialMove or t" is pressed')
+                t=0
+##          if event.key==K_UP:
+##              print('"Up arrow" key pressed')
+    if r==1:
+        print(x)
+        screen.blit(RunForward[x],(540,320))
+        x=x+1
+        if x>=3:
+            x=0
+#            print(x)
+    elif p==1:
+#        print(Punch)
+        screen.blit(Punch[x],(540,320))
+        x=x+1
+        if x>=2:
+            x=0
+    elif s==1:
+        screen.blit(Jump[x],(540,320))
+        x=x+1
+        if x>=1:
+            x=0
+#            print(Jump,x)
+    elif f==1:
+#        print(x)
+        screen.blit(Duck[x],(540,320))
+        x=x+1
+        if x>=1:
+            x=0
+    elif q==1:
+        screen.blit(Slide[x],(540,320))
+        x=x+1
+        if x>=1:
+            x=0
+    elif t==1:
+        print(x)
+        screen.blit(SpecialMove[x],(540,320))
+        x=x+1
+        if x>=1:
+            x=0
+    else:
+        screen.blit(l[0],(540,320))
+
