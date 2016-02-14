e4ae2913e5e1c780c301cfc9ed89e0e90664a197
Chaitanya Nookala <chaitanya.nookala@outlook.com>
2016-02-14 17:44:42 +0000

    Cleaned up all of the comments.

--- streetfighter.py
+++ streetfighter.py
@@ -3,36 +3,8 @@ screen=pygame.display.set_mode((1080,640))
 from pygame.locals import*
 pygame.display.set_caption('Street Fighter')
 from streetfighterclass import Sfighter
-'''img0=pygame.image.load('images/p0.png')
-img1=pygame.image.load('images/p1.png')
-img2=pygame.image.load('images/p2.png')
-img3=pygame.image.load('images/p3.png')
-img4=pygame.image.load('images/p4.png')
-
-l=[img0,img1,img2,img3,img4]'''
-'''l=[]
-Jump=[]
-RunForward=[]
-Punch=[]
-Duck=[]
-Slide=[]
-SpecialMove=[]
-for a in range(0,19,1):
-    l.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
-    if a==7 or a==18:
-        Punch.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
-    if a==6:
-        Jump.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
-    if a==3 or a==4 or a==5:
-        RunForward.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
-    if a==9:
-        Duck.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
-    if a==8:
-        Slide.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
-    if a==10:
-        SpecialMove.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
-
-'''
+S1=Sfighter()
+clock=pygame.time.Clock()
 x=0
 r=0
 p=0
@@ -42,10 +14,6 @@ d=0
 f=0
 q=0
 t=0
-
-S1=Sfighter()
-
-clock=pygame.time.Clock()
 while True:
     clock.tick(5)
     pygame.display.update()
@@ -56,100 +24,47 @@ while True:
             exit()
         if event.type==KEYDOWN:
             if event.key==K_d:
-                print('"Run or D" key pressed')
                 r=1
-               # S1.Spritecount=0
-               # S1.Move(screen,S1.RunForward)
             if event.key==K_e:
-                print('"Punch or E" key pressed')
                 p=1
             if event.key==K_SPACE:
-                print('"Jump or SPACEBAR" key pressed')
                 s=1
-                #S1.Spritecount=0
             if event.key==K_f:
-                print('"Duck or F" key pressed')
                 f=1
-                #S1.Spritecount=0
-               # S1.Move(screen,S1.Duck)
             if event.key==K_q:
-                print('"Slide or q" is pressed')
                 q=1
             if event.key==K_t:
-                print('"SpecialMove or t" is pressed')
                 t=1
-               # S1.Spritecount=0
-               # S1.Move(screen,S1.SpecialMove)
-            if event.key==K_UP:
-              print ('"Up" arrow" key pressed')
         if event.type==KEYUP:
             if event.key==K_d:
-                print('"Run or D" key pressed')
                 r=0
             if event.key==K_e:
-                print('"Punch or E" key pressed')
                 p=0
             if event.key==K_SPACE:
-                print('"Jump or SPACEBAR" key pressed')
                 s=0
             if event.key==K_f:
-                print('"Duck or F" key pressed')
                 f=0
             if event.key==K_q:
-                print('"Slide or q" is pressed')
                 q=0
             if event.key==K_t:
-                print('"SpecialMove or t" is pressed')
                 t=0
-            if event.key==K_UP:
-              print('"Up arrow" key pressed')
     if r==1:
-        #S1.Spritecount=0
         S1.Move(screen,S1.RunForward)
-        '''print(x)
-        screen.blit(RunForward[x],(540,320))
-        x=x+1
-        if x>=3:
-            x=0
-            print(x)'''
     elif p==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Punch)        
-        '''print(Punch)
-        screen.blit(Punch[x],(540,320))
-        x=x+1
-        if x>=2:
-            x=0'''
     elif s==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Jump)
-        '''screen.bdflit(Jump[0],(540,320))
-        x=x+1
-        if x>=1:
-        x=0
-        print(Jump,x)'''
     elif f==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Duck)
-        '''print(x)
-        screen.blit(Duck[0],(540,320))
-        x=x+1
-        if x>=1:
-        x=0'''
     elif q==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Slide)
-        '''screen.blit(Slide[0],(540,320))
-        if x>=1:
-        x=0'''
     elif t==1:
         S1.Spritecount=0
         S1.Move(screen,S1.SpecialMove)
-        '''print(0)
-        screen.blit(SpecialMove[0],(540,320))
-        x=x+1
-        if x>=1:
-        x=0'''
     else:
         screen.blit(S1.l[0],(540,320))
 
--- streetfighterclass.py
+++ streetfighterclass.py
@@ -10,29 +10,25 @@ class Sfighter:
         self.SpecialMove=[]
         self.Spritecount=0
         for a in range(0,19,1):
-            self.l.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+            self.l.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))
             if a==7 or a==18:
-                self.Punch.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+                self.Punch.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))
             if a==6:
-                self.Jump.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+                self.Jump.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))
             if a==3 or a==4 or a==5:
-                self.RunForward.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
+                self.RunForward.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))    
             if a==9:
-                self.Duck.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))    
+                self.Duck.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))    
             if a==8:
-                self.Slide.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+                self.Slide.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))
             if a==10:
-                self.SpecialMove.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(100,100)))
+                self.SpecialMove.append(pygame.transform.scale(pygame.image.load('images/p'+str(a)+'.png'),(125,125)))
     def Move(self,screen,Spritelist):
         self.Spritelist=Spritelist
         if len(self.Spritelist)>=1:
-            print(self.Spritecount)
             self.Spritecount=self.Spritecount+1
             if self.Spritecount>=len(self.Spritelist) or len(self.Spritelist)==1:
                 self.Spritecount=0
-                print(self.Spritecount)
-        #self.Spritecount=0
-        print(self.Spritecount,len(self.Spritelist))
         screen.fill((0,0,0))
         screen.blit(self.Spritelist[self.Spritecount],(540,320))
         pygame.display.update()
