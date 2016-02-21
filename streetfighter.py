045dba08cd666249f139451984f4077c5d5725df
suchin.ravi <suchin.ravi@gmail.com>
2016-02-21 10:20:26 -0800

    Changed some variables and images

--- streetfighter.py
+++ streetfighter.py
@@ -4,16 +4,17 @@ from pygame.locals import*
 pygame.display.set_caption('Street Fighter')
 from streetfighterclass import Sfighter
 S1=Sfighter()
+S2=Sfighter()
 clock=pygame.time.Clock()
-x=0
-r=0
-p=0
-s=0
+a=0
+b=0
 c=0
 d=0
+e=0
 f=0
-q=0
-t=0
+g=0
+h=0
+z=0
 while True:
     clock.tick(5)
     pygame.display.update()
@@ -24,45 +25,45 @@ while True:
             exit()
         if event.type==KEYDOWN:
             if event.key==K_d:
-                r=1
+                a=1
             if event.key==K_e:
-                p=1
+                b=1
             if event.key==K_SPACE:
-                s=1
+                e=1
             if event.key==K_f:
                 f=1
-            if event.key==K_q:
-                q=1
-            if event.key==K_t:
-                t=1
+            if event.key==K_w:
+                g=1
+            if event.key==K_r:
+                h=1
         if event.type==KEYUP:
             if event.key==K_d:
-                r=0
+                a=0
             if event.key==K_e:
-                p=0
+                b=0
             if event.key==K_SPACE:
-                s=0
+                e=0
             if event.key==K_f:
                 f=0
-            if event.key==K_q:
-                q=0
-            if event.key==K_t:
-                t=0
-    if r==1:
+            if event.key==K_w:
+                g=0
+            if event.key==K_r:
+                h=0
+    if a==1:
         S1.Move(screen,S1.RunForward)
-    elif p==1:
+    elif b==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Punch)        
-    elif s==1:
+    elif e==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Jump)
     elif f==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Duck)
-    elif q==1:
+    elif g==1:
         S1.Spritecount=0
         S1.Move(screen,S1.Slide)
-    elif t==1:
+    elif h==1:
         S1.Spritecount=0
         S1.Move(screen,S1.SpecialMove)
     else:
