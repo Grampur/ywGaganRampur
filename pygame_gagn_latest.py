6fddea123476bf7edb406d1c337c04c855845305
Vasavi Nookala <vasavi.nookala@gmail.com>
2016-03-27 16:50:24 +0000

    Revert "Got the moves to work."
    This reverts commit cc52ddf67201eb684df9b333fba1e713982044ae.

---
+++ pygame_gagn_latest.py
@@ -0,0 +1,248 @@
+import pygame
+from pygame.locals import *
+screen=pygame.display.set_mode((640,480))
+pygame.display.set_caption('Menu')
+pygame.init()
+fpsClock=pygame.time.Clock()
+#COLOR GROUP
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+white=(255,255,255)
+black=(0,0,0)
+def button(msg,ic,ac,x,y,w,h,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed() 
+    if x+w > mouse[0]>x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h)) 
+    show_text( msg, blue,x+w/2,y+h/2)
+def show_text(msg,color,x,y):
+    fontobj= pygame.font.SysFont('comicsans',20)
+    msgobj = fontobj.render(msg,False,color)
+    a,b,w,h=msgobj.get_rect()
+    screen.blit(msgobj,(x-w/2,y-h/2))
+#Game Function . This funtion is called by the Main while loop (Menu loop)
+#This function returns to the menu loop 
+#Similar functions can be implemeted for pause and game over menus
+def game1player():
+    from random import randint
+    from time import sleep
+
+    screen=pygame.display.set_mode((640,480))
+    pygame.display.set_caption('Ping Pong!')
+    pygame.init()
+#PADDLE DIMENSIONS
+    leftpaddlex=150
+    leftpaddley=300
+    rightpaddlex=450
+    rightpaddley=350
+#PADDLE Y/X
+    r_pad_up=0
+    r_pad_down=0
+    l_pad_up=0
+    l_pad_down=0
+    pspeed=3.5
+    circlex=310
+    circley=280
+    red=(255,0,0)
+    green=(0,255,0)
+    blue=(0,0,255)
+    white=(255,255,255)
+    black=(0,0,0)
+    screen.fill(white)
+    speedx=2
+    speedy=0
+    scoreleft=0
+    scoreright=0
+    def show_text(msg,x,y,color):
+        fontobj=pygame.font.SysFont('freesans',32)
+        msgobj=fontobj.render(msg,False,color)
+        screen.blit(msgobj,(x,y))
+    while True:    
+        pygame.display.update()
+        fpsClock.tick(120)
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen,(0,12,113),(120,60,400,400))
+        pygame.draw.circle(screen,white,(circlex,circley),5,3)
+        pygame.draw.line(screen,green,(leftpaddlex,leftpaddley),(leftpaddlex,leftpaddley+80),3)
+        pygame.draw.line(screen,green,(rightpaddlex,rightpaddley),(rightpaddlex,rightpaddley+80),3)
+        show_text('The Score is',210,5,red)
+        show_text(str(scoreleft)+'-'+str(scoreright),277,29,red)
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                exit()
+            elif event.type==KEYDOWN:
+                if event.key==K_DOWN:
+                    r_pad_down=1
+                elif event.key==K_UP:
+                    r_pad_up=1
+            elif event.type==KEYUP:
+                if event.key==K_DOWN:
+                    r_pad_down=0
+                elif event.key==K_UP:
+                    r_pad_up=0
+        #Ball Movement
+        circlex=circlex+speedx
+        circley=circley+speedy
+        if circlex==rightpaddlex and rightpaddley<=circley<=(rightpaddley+70):
+            #print('collision with right paddle detected')
+            speedx=-speedx
+            speedy=randint(-1,1 )
+        if circlex==leftpaddlex and leftpaddley<=circley<=(leftpaddley+70):
+            #print('collision with left paddle detected')
+            speedx=-speedx
+        if circley<=60:
+            speedy=-speedy
+        if circley>=450:
+            speedy=-speedy
+        if circlex>=520:
+            circlex=310
+            scoreleft=scoreleft+1
+        if circlex<=60:
+            circlex=310
+            scoreright=scoreright+1
+        #paddle limits
+        if r_pad_up==1:
+           if rightpaddley>=65:
+                rightpaddley=rightpaddley-pspeed
+        if r_pad_down==1:
+            if rightpaddley<=376:
+                rightpaddley=rightpaddley+pspeed      
+        #####AI
+        if circley > leftpaddley:
+            l_pad_down=1
+            l_pad_up=0
+            if leftpaddley>=376:
+                l_pad_down=0
+            #print("lpadup",l_pad_up,"lpaddown",l_pad_down,pspeed,leftpaddley,circlex,circley)
+        if circley < leftpaddley:
+            l_pad_up=1
+            l_pad_down=0
+            if leftpaddley<=65:
+                 l_pad_up=0
+            #print("lpadup",l_pad_up,"lpaddown",l_pad_down,pspeed,leftpaddley,circlex,circley)
+        if l_pad_up==1:
+            leftpaddley=leftpaddley-pspeed
+        if l_pad_down==1:
+            leftpaddley=leftpaddley+pspeed
+    if circley == leftpaddley:
+            l_pad_up=0
+            l_pad_down=0
+def game2player():
+    from random import randint
+    from time import sleep
+    pygame.display.set_caption('Ping Pong!')
+    leftpaddlex=150
+    pygame.init()
+    leftpaddley=300
+    rightpaddlex=450
+    rightpaddley=350
+    r_pad_up=0
+    r_pad_down=0
+    l_pad_up=0
+    l_pad_down=0
+    pspeed=4
+    circlex=310
+    circley=280
+    red=(255,0,0)
+    green=(0,255,0)
+    blue=(0,0,255)
+    white=(255,255,255)
+    black=(0,0,0)
+    screen.fill(white)
+    speedx=2
+    speedy=0
+    scoreleft=0
+    scoreright=0
+    while score==21:
+    def show_text(msg,x,y,color):
+        fontobj=pygame.font.SysFont('freesans',32)
+        msgobj=fontobj.render(msg,False,color)
+        screen.blit(msgobj,(x,y))
+    def GAME_OVER(msg,x,y,color):
+        fontobj=pygame.font.SysFont('freesans',320)
+        msgobj=fontobj.render(msg,False,color)
+        screen.blit(msgobj,(x,y))
+    while True:    
+        pygame.display.update()
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen,(0,12,113),(120,60,400,400))
+        pygame.draw.circle(screen,white,(circlex,circley),5,3)
+        pygame.draw.line(screen,green,(leftpaddlex,leftpaddley),(leftpaddlex,leftpaddley+80),3)
+        pygame.draw.line(screen,green,(rightpaddlex,rightpaddley),(rightpaddlex,rightpaddley+80),3)
+        show_text('The Score is',210,5,red)
+        show_text(str(scoreleft)+'-'+str(scoreright),277,29,red)
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                exit()
+            elif event.type==KEYDOWN:
+                if event.key==K_DOWN:
+                    r_pad_down=1
+                elif event.key==K_UP:
+                    r_pad_up=1
+                if event.key==K_s:
+                    l_pad_up=1
+                elif event.key==K_w:
+                    l_pad_down=1
+            elif event.type==KEYUP:
+                if event.key==K_DOWN:
+                    r_pad_down=0
+                elif event.key==K_UP:
+                    r_pad_up=0
+                if event.key==K_s:
+                    l_pad_up=0
+                elif event.key==K_w:
+                    l_pad_down=0
+        #Ball Movement
+        circlex=circlex+speedx
+        circley=circley+speedy
+        if circlex==rightpaddlex and rightpaddley<=circley<=(rightpaddley+70):
+            #print('collision with right paddle detected')
+            speedx=-speedx
+            speedy=randint(-2,2 )
+        if circlex==leftpaddlex and leftpaddley<=circley<=(leftpaddley+70):
+            #print('collision with left paddle detected')
+            speedx=-speedx
+        if circley<=60:
+            speedy=-speedy
+        if circley>=450:
+            speedy=-speedy
+        if circlex>=520:
+            circlex=310
+            scoreleft=scoreleft+1
+        if circlex<=60:
+            circlex=310
+            scoreright=scoreright+1
+        #paddle limits
+        if r_pad_up==1:
+            if rightpaddley>=65:
+                rightpaddley=rightpaddley-pspeed
+        if r_pad_down==1:
+            if rightpaddley<=356:
+                rightpaddley=rightpaddley+pspeed
+        if l_pad_down==1:
+            if leftpaddley>=65:
+                leftpaddley=leftpaddley-pspeed
+        if l_pad_up==1:
+            if leftpaddley<=356:
+                leftpaddley=leftpaddley+pspeed   
+    screen.fill(255,255,255)
+    GAME_OVER('GAME OVER',210,55,red)
+#This is the main menu loop 
+while True:
+    screen.fill((0,0,0)) #Black
+    button('1-Player',red,green,250,350,100,50,game1player)
+    button('2-Player',red,green,50,350,100,50,game2player)
+    button("QUIT",green,red,450,350,100,50,exit)
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
---
+++ streetfighter.py
@@ -0,0 +1,144 @@
+import pygame
+screen=pygame.display.set_mode((1080,640))
+from pygame.locals import*
+pygame.display.set_caption('Street Fighter')
+from streetfighterclass import Sfighter
+S1=Sfighter('p',140,440)
+S2=Sfighter('pD',780,440)
+clock=pygame.time.Clock()
+Background=pygame.transform.scale(pygame.image.load('images/SR6.gif'),(1080,640))
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+i=0
+k=0
+j=0
+l=0
+m=0
+p1right=0
+p1left=0
+p2left=0
+p2right=0
+while True:
+    clock.tick(5)
+    pygame.display.update()
+    screen.fill((0,0,0))
+    screen.blit(Background,(0,0))
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            exit()
+        if event.type==KEYDOWN:
+          #  if event.key==K_d:
+           #     a=1
+            if event.key==K_e:
+                b=1
+            if event.key==K_SPACE:
+                e=1
+            if event.key==K_f:
+                f=1
+            if event.key==K_w:
+                g=1
+            if event.key==K_r:
+                h=1
+            if event.key==K_k:
+                k=1
+         #   if event.key==K_j:
+          #      j=1
+            if event.key==K_l:
+                l=1
+            if event.key==K_m:
+                m=1
+            if event.key==K_d:
+                p1right=1
+                S1.direction=0
+                S1.xchange=-30
+            if event.key==K_a:
+                p1left=1
+                S1.direction=1
+                S1.xchange=30
+            if event.key==K_LEFT:
+                p2left=1
+                S2.direction=0
+                S2.xchange=-30
+            if event.key==K_RIGHT:
+                p2right=1
+                S2.direction=1
+                S2.xchange=30
+        if event.type==KEYUP:
+           # if event.key==K_d:
+            #    a=0
+            if event.key==K_e:
+                b=0
+            if event.key==K_SPACE:
+                e=0
+            if event.key==K_f:
+                f=0
+            if event.key==K_w:
+                g=0
+            if event.key==K_r:
+                h=0
+            if event.key==K_k:
+                k=0
+          #  if event.key==K_j:
+           #     j=0
+            if event.key==K_l:
+                l=0
+            if event.key==K_m:
+                m=0
+            if event.key==K_d:
+                p1right=0
+            if event.key==K_a:
+                p1left=0
+            if event.key==K_LEFT:
+                p2left=0
+            if event.key==K_RIGHT:
+                p2right=0
+    if p1right==1:
+        S1.Move(screen,S1.RunForward)
+        S1.startx=S1.startx-S1.xchange
+    elif p1left==1:
+        S1.Move(screen,S1.RunForward)
+        S1.startx=S1.startx-S1.xchange
+    elif b==1:
+        S1.Spritecount=0 
+        S1.Move(screen,S1.Punch)        
+    elif e==1:
+        S1.Spritecount=0
+        S1.Move(screen,S1.Jump)
+    elif f==1:
+        S1.Spritecount=0
+        S1.Move(screen,S1.Duck)
+    elif g==1:
+        S1.Spritecount=0
+        S1.Move(screen,S1.Slide)
+    elif h==1:
+        S1.Spritecount=0
+        S1.Move(screen,S1.SpecialMove)
+    else:
+        if S1.direction==0:
+            screen.blit(S1.l[0],(S1.startx,S1.starty))
+        else:
+            screen.blit(pygame.transform.flip(S1.l[0],True,False),(S1.startx,S1.starty))
+    if k==1:               
+        S2.Move(screen,S2.Jump)
+    elif p2left==1:
+        S2.Move(screen,S2.RunForward)
+        S2.startx=S2.startx+S2.xchange
+    elif p2right==1:
+        S2.Move(screen,S2.RunForward)
+        S2.startx=S2.startx+S2.xchange
+    elif l==1:
+        S2.Move(screen,S2.Duck)
+    elif m==1:
+        S2.Move(screen,S2.SpecialMove)
+    else:
+        if S2.direction==0:
+            screen.blit(S2.l[0],(S2.startx,S2.starty))
+        else:
+            screen.blit(pygame.transform.flip(S2.l[0],True,False),(S2.startx,S2.starty))
