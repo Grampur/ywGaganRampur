e2ccbf21c912b39adb920ec8ffdca3d565151c47
WillMarg <awesomewillisawesome@gmail.com>
2016-04-17 10:20:31 -0700

    added a forever loop to the ultrasensor

--- GaganSensor.py
+++ GaganSensor.py
@@ -7,17 +7,17 @@ GPIO.setup(GPIO_TRIGGER,GPIO.OUT)       #Trigger
 GPIO.setup(GPIO_ECHO,GPIO.IN)           #Echo
 GPIO.output(GPIO_TRIGGER,False)
 time.sleep(0.5)
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER,False)
-start=time.time()
-while GPIO.input(GPIO_ECHO)==0:
+while True:
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER,False)
     start=time.time()
-    #print('1st')
-while GPIO.input(GPIO_ECHO)==1:
-    stop=time.time()
-elapsed=stop-start
-distance=elapsed*34000
-distance=distance/2
-print('Distance :',distance)
+    while GPIO.input(GPIO_ECHO)==0:
+        start=time.time()
+    while GPIO.input(GPIO_ECHO)==1:
+        stop=time.time()
+    elapsed=stop-start
+    distance=elapsed*34000
+    distance=distance/2
+    print('Distance :',distance)
 GPIO.cleanup()
