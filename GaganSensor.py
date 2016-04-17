ec735b99084ceea506a7fd2130f19b2c3b38e7c5
WillMarg <awesomewillisawesome@gmail.com>
2016-04-17 10:10:44 -0700

    Ultrasonic sensor is now working

--- GaganSensor.py
+++ GaganSensor.py
@@ -1,20 +1,23 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO_TRIGGER=12
 GPIO_ECHO=6
 GPIO.setup(GPIO_TRIGGER,GPIO.OUT)       #Trigger    
 GPIO.setup(GPIO_ECHO,GPIO.IN)           #Echo
 GPIO.output(GPIO_TRIGGER,False)
-sleep(0.5)
-GPIO.output(GPIO_TRIGGER, False)
-sleep(0.00001)
+time.sleep(0.5)
+GPIO.output(GPIO_TRIGGER, True)
+time.sleep(0.00001)
 GPIO.output(GPIO_TRIGGER,False)
 start=time.time()
 while GPIO.input(GPIO_ECHO)==0:
     start=time.time()
+    #print('1st')
 while GPIO.input(GPIO_ECHO)==1:
     stop=time.time()
 elapsed=stop-start
 distance=elapsed*34000
 distance=distance/2
+print('Distance :',distance)
+GPIO.cleanup()
