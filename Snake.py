ee94d2c1666053213742ac89cd76ac9805db536a
Grampur <grampur123@gmail.com>
2016-07-22 01:20:04 +0000

    Created food, added movement, and detected collision

--- Snake.py
+++ Snake.py
@@ -1,17 +1,60 @@
 import pygame
-pygame.display.locals import *
+from pygame.locals import *
 from random import randint
 pygame.init()
-screen.pygame.display.set.mode((640,1080))
-foodx=randint((1,10))
-foody=randint((1,10))
-snakex=(random.randint(0,640) // 10)*10
-snakey=(random.randint(0,640) // 10)*10
+clock=pygame.time.Clock()
+screen=pygame.display.set_mode((640,640))
+foodx=(randint(0,640) // 10)*10
+foody=(randint(0,640) // 10)*10
+snakex=(randint(0,640) // 10)*10
+snakey=(randint(0,640) // 10)*10
+pygame.display.set_caption('Snake')
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+up=0
+down=0
+left=0
+right=0
 while True:
+    clock.tick(15)
     pygame.display.update()
-    pygame.display.set_caption('Snake')
-    pygaem.draw.rect(screen,red,(50,50,10,10),5)
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,red,(foodx,foody,10,10),5)
+    pygame.draw.rect(screen,green,(snakex,snakey,10,10),5)
+    if up==1:
+        snakey=snakey-10
+    if down==1:
+        snakey=snakey+10
+    if left==1:
+        snakex=snakex-10
+    if right==1:
+        snakex=snakex+10
+    print(up,down,left,right)
+    if (foodx,foody)==(snakex,snakey):
+        print('collision')
     for event in pygame.event.get():
+        if event.type==KEYDOWN:
+            if event.key==K_w:
+                up=1
+                down=0
+                left=0
+                right=0
+            if event.key==K_s:
+                down=1
+                up=0
+                right=0
+                left=0
+            if event.key==K_a:
+                left=1
+                down=0
+                up=0
+                right=0
+            if event.key==K_d:
+                right=1
+                up=0
+                down=0
+                left=0
         if event.type==QUIT:
             pygame.quit()
             exit()
