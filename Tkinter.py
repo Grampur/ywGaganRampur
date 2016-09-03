e56ec3768a0b971e8434000cfab6c16ee12451de
Gagan Rampur <Gagan Rampur>
2016-09-02 18:22:17 -0700

    Started new excersise countdown

--- Tkinter.py
+++ Tkinter.py
@@ -10,34 +10,73 @@
 
 
 
+##from tkinter import *
+##master=Tk()
+##master.title('')
+##def Printtext(event):
+##    fn=e1.get()
+##    ln=e2.get()
+##    e1.delete(0,END)
+##    e2.delete(0,END)
+##    print(fn,ln)
+##def Onclick ():
+##    fn=e1.get()
+##    ln=e2.get()
+##    e1.delete(0,END)
+##    e2.delete(0,END)
+##    print(fn,ln)
+##l1=Label(master,text='First Name')
+##l2=Label(master,text='Last Name')
+##e1=Entry(master)
+##e2=Entry(master)
+##b1=Button(master,text='Show name',command=Onclick)
+##b2=Button(master,text='Quit', command=master.quit)
+##l1.grid(row=0,column=0)
+##l2.grid(row=1,column=0)
+##e1.grid(row=2,column=0)
+##e2.grid(row=3,column=0)
+##b1.grid(row=4,column=0)
+##b2.grid(row=5,column=0)
+##b1.bind('<Return>', Printtext)
+##while True:
+##    master.update()
+###mainloop()
+
 from tkinter import *
 master=Tk()
 master.title('')
-def Printtext(event):
-    fn=e1.get()
-    ln=e2.get()
-    e1.delete(0,END)
-    e2.delete(0,END)
-    print(fn,ln)
+a=IntVar()
+stcount=0
+stpcount=0
+from time import sleep
 def Onclick ():
-    fn=e1.get()
-    ln=e2.get()
-    e1.delete(0,END)
-    e2.delete(0,END)
-    print(fn,ln)
-l1=Label(master,text='first Name')
-l2=Label(master,text='last Name')
+    global stcount
+    a.set(e1.get())
+    stcount=1
+    print('hi')
+def stopcountdown():
+    global stpcount
+    stpcount=1
+l1=Label(master,textvariable=a)
 e1=Entry(master)
-e2=Entry(master)
-b1=Button(master,text='Show name',command=Onclick)
-b2=Button(master,text='Quit', command=master.quit)
+b1=Button(master,text='Start countdown',command=Onclick)
+b2=Button(master,text='Stop countdown',command=stopcountdown)
 l1.grid(row=0,column=0)
-l2.grid(row=1,column=0)
-e1.grid(row=2,column=0)
-e2.grid(row=3,column=0)
-b1.grid(row=4,column=0)
-b2.grid(row=5,column=0)
-b1.bind('<Return>', Printtext)
+e1.grid(row=1,column=0)
+b1.grid(row=2,column=0)
+b2.grid(row=3,column=0)
 while True:
+    print('Hola', stcount)
+    if a.get()>0 and stcount==1:
+        a.set(a.get()-1)
+        print('hello')
+        #sleep(1)
+    if stpcount==1:
+        stcount=0
+        
     master.update()
-#mainloop()
+    
+
+
+
+
